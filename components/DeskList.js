f981be656ff63dc8fac80ee1a2361aea7ca654d5
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2019-05-15 22:03:11 -0300

    Add scrollable list and click handle to decks

--- components/DeskList.js
+++ components/DeskList.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import DeckItem from './DeckItem';
+import React, { Component } from "react";
+import { View, TouchableOpacity } from "react-native";
+import DeckItem from "./DeckItem";
+import { FlatList } from "react-native-gesture-handler";
 
 export default class DeckList extends Component {
+  goToDeckPage() {
+    alert("go to deck page");
+  }
+
+  renderItem({ item }) {
+    return (
+      <TouchableOpacity onPress={this.goToDeckPage}>
+        <DeckItem deck={item} />
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     const { decks } = this.props;
 
     return (
       <View>
-        {decks.map((deck, i) => <DeckItem key={i} deck={deck} />)}
+        <FlatList
+          data={decks}
+          renderItem={this.renderItem.bind(this)}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
-    )
+    );
   }
 }
--- screens/DeckListScreen.js
+++ screens/DeckListScreen.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import DeskList from '../components/DeskList';
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import DeskList from "../components/DeskList";
 
 export default class DeckListScreen extends Component {
   render() {
     const decks = [
-      { title: "React", cardCount: 5 },
-      { title: "React Native", cardCount: 3 },
-      { title: "Vue.js", cardCount: 1 },
-    ]
+      { id: 1, title: "React", cardCount: 5 },
+      { id: 2, title: "React Native", cardCount: 3 },
+      { id: 3, title: "Vue.js", cardCount: 1 },
+      { id: 4, title: "React", cardCount: 5 },
+      { id: 5, title: "React Native", cardCount: 3 },
+      { id: 6, title: "Vue.js", cardCount: 1 },
+      { id: 7, title: "React", cardCount: 5 },
+      { id: 8, title: "React Native", cardCount: 3 },
+      { id: 9, title: "Vue.js", cardCount: 1 },
+      { id: 10, title: "React", cardCount: 5 },
+      { id: 11, title: "React Native", cardCount: 3 },
+      { id: 12, title: "Vue.js", cardCount: 1 },
+      { id: 13, title: "React", cardCount: 5 },
+      { id: 14, title: "React Native", cardCount: 3 },
+      { id: 15, title: "Vue.js", cardCount: 1 }
+    ];
     return (
       <View style={styles.container}>
         <DeskList decks={decks} />
@@ -20,8 +32,8 @@ export default class DeckListScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingLeft: 20,
-    paddingRight: 20,
+    paddingRight: 20
   }
 });
