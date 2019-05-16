f60acf4194c4221fbb8da515f1178ea8d6016487
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2019-05-15 22:52:12 -0300

    Add deck form screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,39 +1,52 @@
-import React from 'react';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import LinksScreen from '../screens/LinksScreen';
-import DeckListScreen from '../screens/DeckListScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import LinksScreen from "../screens/LinksScreen";
+import DeckListScreen from "../screens/DeckListScreen";
+import NewDeckListScreen from "../screens/NewDeckListScreen";
 
 const DeckStack = createStackNavigator({
-  Deck: DeckListScreen,
+  Deck: DeckListScreen
 });
 
 DeckStack.navigationOptions = {
-  tabBarLabel: 'Decks',
+  tabBarLabel: "Decks",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={`cards${focused ? "" : "-outline"}`} />
+  )
+};
+
+const NewDeckStack = createStackNavigator({
+  NewDeck: NewDeckListScreen
+});
+
+NewDeckStack.navigationOptions = {
+  tabBarLabel: "New Deck",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={`cards${focused ? '' : '-outline'}`}
+      name={`ios-add-circle${focused ? "" : "-outline"}`}
     />
-  ),
+  )
 };
 
 const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+  Links: LinksScreen
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: "Links",
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={'ios-link'}
-    />
-  ),
+    <TabBarIcon focused={focused} name={"ios-link"} />
+  )
 };
 
 export default createBottomTabNavigator({
   DeckStack,
-  LinksStack,
+  NewDeckStack,
+  LinksStack
 });
---
+++ screens/NewDeckListScreen.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  TouchableOpacity
+} from "react-native";
+
+export default class NewDeckScreen extends React.Component {
+  state = {
+    title: ""
+  };
+
+  handleTitleChange(title) {
+    this.setState({ title });
+  }
+
+  handleAddClick() {
+    alert("add desk");
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        <Text style={styles.label}>What is the title of your new desk?</Text>
+        <TextInput
+          style={styles.input}
+          value={this.state.title}
+          onChangeText={this.handleTitleChange.bind(this)}
+        />
+        <TouchableOpacity style={styles.button} onPress={this.handleAddClick}>
+          <Text style={styles.buttonText}>Add Desk</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  label: {
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    fontSize: 40,
+    marginBottom: 20
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#163eb7",
+    borderWidth: 1,
+    borderColor: "#fff",
+    marginTop: 30,
+    width: 100,
+    height: 50
+  },
+  buttonText: {
+    color: "#fff"
+  }
+});
